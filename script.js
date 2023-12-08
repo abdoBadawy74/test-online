c4bd30f927f0dca9cd4ab40378e57ed6f3d21abf
abd074 <badawyabdulrahman099@gmail.com>
2023-12-08 18:19:04 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,38 @@
+let title = document.querySelector(".title");
+
+let ul = document.querySelector("ul");
+
+let reload = document.querySelector(".reload");
+
+window.onload = function () {
+  if (window.navigator.online) {
+    online();
+  } else {
+    offline();
+  }
+};
+
+window.addEventListener("online", function () {
+  online();
+});
+window.addEventListener("offline", function () {
+  offline();
+});
+
+reload.onclick = function () {
+  window.location.reload();
+};
+
+function online() {
+  title.innerHTML = "Online Now";
+  title.style.color = "green";
+  ul.classList.add("hide");
+  reload.classList.add("hide");
+}
+
+function offline() {
+  title.innerHTML = "Offline Now";
+  title.style.color = "#666";
+  ul.classList.remove("hide");
+  reload.classList.remove("hide");
+}
